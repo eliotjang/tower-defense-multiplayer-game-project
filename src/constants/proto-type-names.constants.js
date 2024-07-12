6c0dbea60c9b6fe82bdf1c1902fc666f0f976f1d
Dongkyun Kim <donkim0122@gmail.com>
2024-07-12 18:16:42 +0900

    Update: proto 파일, 매핑, 클래스 추가 및 변경

--- src/constants/proto-type-names.constants.js
+++ src/constants/proto-type-names.constants.js
@@ -1,11 +1,8 @@
 const protoTypeNames = {
-  SIGN_UP_REQUEST: "C2S_SignUpRequest",
-  SIGN_UP_RESPONSE: "S2C_SignUpResponse",
-  LOG_IN_REQUEST: "C2S_LoginRequest",
-  LOG_IN_RESPONSE: "S2C_LoginResponse",
-  MATCH_REQUEST: "C2S_MatchRequest",
-  MATCH_FOUND: "S2C_MatchFound",
-  GAME_PACKET: "GamePacket",
+  // GAME_PACKET: "GamePacket",
+  REQUEST: "RequestPacket",
+  RESPONSE: "ResponsePacket",
+  NOTIFICATION: "NotificationPacket",
 };
 
 export default protoTypeNames;
