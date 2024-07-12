242a1925b3bd33aba618b72c1a6cafe29d798980
Dongkyun Kim <donkim0122@gmail.com>
2024-07-12 18:17:50 +0900

    Update: 패킷 종류별 직렬화/역직렬화 함수 추가

--- src/utils/packet-serializer.utils.js
+++ src/utils/packet-serializer.utils.js
@@ -1,18 +1,90 @@
-import protoTypeNames from "../constants/proto-type-names.constants.js";
+import packetNames from "../constants/packet-names.constants.js";
 import { getProtoMessages } from "../init/proto.init.js";
 
-const GamePacket = getProtoMessages()[protoTypeNames.GAME_PACKET];
-
-export const serialize = (packetType, payload) => {
+/**
+ *
+ * @deprecated
+ * @param {*} packetType
+ * @param {*} payloadData
+ * @returns
+ */
+export const serialize = (packetType, payloadData) => {
   // const MessageType = getProtoMessages()[protoTypeNames[packetType].response];
   const encoded = GamePacket.encode(payload).finish();
 
   return encoded;
 };
 
-export const deserialize = (packetType, payload) => {
+/**
+ *
+ * @param {} param0
+ * @returns
+ */
+export const serializeRequest = ({ packetType, token, clientVersion, payload }) => {
+  const RequestPacket = getProtoMessages()[packetNames.packet.request];
+  const payloadData = {
+    packetType,
+    token,
+    clientVersion,
+    [packetNames.payload[packetType].key]: payload,
+  };
+  const serialized = RequestPacket.encode(payloadData).finish();
+  return serialized;
+};
+
+export const serializeResponse = ({ packetType, success, failCode, message, payload }) => {
+  const ResponsePacket = getProtoMessages()[packetNames.packet.response];
+  const payloadData = {
+    packetType,
+    success,
+    failCode,
+    message,
+    [packetNames.payload[packetType].key]: payload,
+  };
+  const serialized = ResponsePacket.encode(payloadData).finish();
+  return serialized;
+};
+
+export const serializeNotification = ({ packetType, timestamp, message, payload }) => {
+  const NotificationPacket = getProtoMessages()[packetNames.packet.notification];
+  const payloadData = {
+    packetType,
+    timestamp,
+    message,
+    [packetNames.payload[packetType].key]: payload,
+  };
+  const serialized = NotificationPacket.encode(payloadData).finish();
+  return serialized;
+};
+
+/**
+ *
+ * @deprecated
+ * @param {*} packetType
+ * @param {*} payloadData
+ * @returns
+ */
+export const deserialize = (packetType, payloadData) => {
   // const MessageType = getProtoMessages()[protoTypeNames[packetType].request];
   const decoded = GamePacket.decode(payload);
 
   return decoded;
+};
+
+export const deserializeRequest = (packet) => {
+  const RequestPacket = getProtoMessages()[packetNames.packet.request];
+  const deserialized = RequestPacket.decode(packet);
+  return deserialized;
+};
+
+export const deserializeResponse = (packet) => {
+  const ResponsePacket = getProtoMessages()[packetNames.packet.response];
+  const deserialized = ResponsePacket.decode(packet);
+  return deserialized;
+};
+
+export const deserializeNotification = (packet) => {
+  const NotificationPacket = getProtoMessages()[packetNames.packet.notification];
+  const deserialized = NotificationPacket.decode(packet);
+  return deserialized;
 };
