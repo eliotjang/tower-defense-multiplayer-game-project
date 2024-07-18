9689565f379130a8544eb8d11e7cfb011ed9d8ae
Dongkyun Kim <donkim0122@gmail.com>
2024-07-18 11:57:15 +0900

    Update: modal 메세지 함수화

--- public/src/modals/modal.js
+++ public/src/modals/modal.js
@@ -1,11 +1,23 @@
 // const modalOpenButton = document.getElementById('modalOpenButton');
-const modalCloseButton = document.getElementById('modalCloseButton');
+// const modalCloseButton = document.getElementById('modalCloseButton');
 const modal = document.getElementById('modalContainer');
+const modalMessage = document.getElementById('modal-message-01');
 
 // modalOpenButton.addEventListener('click', () => {
 //   modal.classList.remove('hidden');
 // });
 
-modalCloseButton.addEventListener('click', () => {
+// modalCloseButton.addEventListener('click', () => {
+//   modal.classList.add('hidden');
+// });
+
+export const hideModal = () => {
   modal.classList.add('hidden');
-});
+};
+
+export const displayModalMessage = (message) => {
+  // const modal = document.getElementById('modalContainer');
+  // const modalMessage = document.querySelector('.modalMessage');
+  modalMessage.innerHTML = message;
+  modal.classList.remove('hidden');
+};
